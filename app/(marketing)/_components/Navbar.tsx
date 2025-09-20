a0282f837a65bc693fda9889e9e484240bab35c0
Ahmed Bahnasy <engineerahmedbahnasy@gmail.com>
2025-09-20 21:41:46 +0300

    feat: completed the authentication  and basic database setup

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -4,22 +4,46 @@ import { cn } from '@/lib/utils'
 import React from 'react'
 import Logo from './Logo'
 import { ModeToggle } from '@/components/ModeToggle'
-
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import Spinner from "@/components/spinner";
+import Link from "next/link";
 
 const Navbar = () => {
-  const scrolled = useScrollTop()
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
   return (
-    
     <nav className={cn("fixed top-0 z-50 flex items-center backdrop-blur-sm transition-all w-full p-6 dark:bg-[#1F1F1F]", scrolled && "border-b shadow-sm")}>
-
       <Logo />
-      <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-6'>
-        
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-6">
+        {isLoading && <Spinner />}
 
-      <ModeToggle/>
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size={"sm"}>
+                Login in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button  size={"sm"}>Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant={"ghost"} size={"sm"} asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSwitchSessionUrl="/" />
+          </>
+        )}
+        <ModeToggle />
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import ConvexClientProvider from "@/components/providers/convex-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,9 +41,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="jotion-theme">
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="jotion-theme">
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
